238b0b083621183807b3573492b7ad035a04e60b
shantanami <s22310550@student.unklab.ac.id>
2024-11-03 01:37:17 +0800

    modify home 3x

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header"
 import Home from "./components/Home"
+import CardInfo from ".components/CardInfo"
 
 const App = () => {
   return (
     <div>
       <Header/>
       <Home/>
+      <CardInfo/>
     </div>
   )
 }
